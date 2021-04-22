c86e0bfdced2b9fc831d7c30568fc7c3b4673384
Xime Moreno <xime0389@hotmail.com>
2021-04-21 21:22:55 -0500

    helpers

---
+++ Urbanizacion.Web/Helpers/IConverterHelper.cs
@@ -0,0 +1,27 @@
+﻿using Urbanizacion.Web.Data.Entities;
+using Urbanizacion.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Urbanizacion.Web.Helpers
+{
+    public interface IConverterHelper
+    {
+        Task<Vehiculostbl> ToVehiculoAsync(VehiculoViewModel model, string path, bool IsNew);
+        Task<Negociostbl> ToNegocioAsync(NegocioViewModel model, string path, bool IsNew);
+        Task<Pagostbl> ToPagoAsync(PagoViewModel model, bool isNew, char pag);
+        Task<Productostbl> ToProductoAsync(ProductoViewModel model, bool IsNew, string path);
+        Task<Noticiastbl> ToNoticiaAsync(NoticiaViewModel model, bool IsNew, string path);
+        Task<Valorestbl> ToValorAsync(ValorViewModel model, bool IsNew);
+        Task<Egresostbl> ToEgresoAsync(EgresoViewModel model, bool IsNew);
+
+        PagoViewModel ToPagoViewModel(Pagostbl pago);
+        VehiculoViewModel ToVehiculoViewModel(Vehiculostbl vehiculo);
+        NegocioViewModel ToNegocioViewModel(Negociostbl negocio);
+        NoticiaViewModel ToNoticiaViewModel(Noticiastbl noticia);
+        ProductoViewModel ToProductoViewModel(Productostbl producto);
+        EgresoViewModel ToEgresoViewModel(Egresostbl egreso);
+    }
+}
---
+++ Urbanizacion.Web/Helpers/IUserHelper.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Urbanizacion.Web.Data.Entities;
+using Urbanizacion.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Urbanizacion.Web.Helpers
+{
+    public interface IUserHelper
+    {
+        Task<Userstbl> GetUserByEmailAsync(string email);
+
+        Task<IdentityResult> AddUserAsync(Userstbl user, string password);
+
+        Task CheckRoleAsync(string roleName);
+
+        Task AddUserToRoleAsync(Userstbl user, string roleName);
+
+        Task<bool> IsUserInRoleAsync(Userstbl user, string roleName);
+
+        Task<SignInResult> LoginAsync(LoginViewModel model);
+
+        Task LogoutAsync();
+
+        Task<bool> DeleteUserAsync(string email);
+
+        Task<IdentityResult> UpdateUserAsync(Userstbl user);
+
+        Task<SignInResult> ValidatePasswordAsync(Userstbl user, string password);
+        Task<string> GenerateEmailConfirmationTokenAsync(Userstbl user);
+
+        Task<IdentityResult> ConfirmEmailAsync(Userstbl user, string token);
+
+        Task<Userstbl> GetUserByIdAsync(string userId);
+
+    }
+}
